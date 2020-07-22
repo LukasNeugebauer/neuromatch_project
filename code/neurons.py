4534be5d2c9ee6fe2f45c90f72a7d08027450835
Lukas Neugebauer <l.neugebauer@uke.de>
2020-07-22 16:17:00 +0200

    First draft for neurons and layers and model structure

---
+++ code/neurons.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+params = {'alpha': 2, 'beta': 3}
+
+class SensoryNeuron:
+    def __init__(
+        self,
+        alpha,
+        beta,
+        *args,
+        **kwargs
+    ):
+        self.alpha = alpha
+        self.beta = beta
+
+SensoryNeuron(**params)
+        
+
+class OpponencyNeuron:
+    def __init__(
+
+    ):
+        
+    
+
+class AttentionNeuron:
+    def __init__(
+
+    ):
+        
+    
+
+class SummationNeuron:
+    def __init__(
+
+    ):
+        
+    
+
