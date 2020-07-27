65fb9a836b24057838ae73db52a9382cde630bb1
NabbefeldG <68555047+NabbefeldG@users.noreply.github.com>
2020-07-27 15:42:25 +0200

    Update get_default_parameters.py
    fixed keys

--- code/get_default_parameters.py
+++ code/get_default_parameters.py
@@ -1,26 +1,26 @@
 def get_default_parameters(attended=True):
     params = {
-        'dt'   ': .5,          # time-step in forward Euler method (ms)
-        'T     ': 15000,       # total duration to simulate (ms)
-        'nt    ': p.T/p.dt+1,  # number of time point
-        'tlist ': 0:p.dt:p.T,  # time vector (ms)
-        'nx    ': 1,           # simulate one location
+        'dt'    : .5,          # time-step in forward Euler method (ms)
+        'T'     : 15000,       # total duration to simulate (ms)
+        'nt'    : p.T/p.dt+1,  # number of time point
+        'tlist' : 0:p.dt:p.T,  # time vector (ms)
+        'nx'    : 1,           # simulate one location
         'ntheta': 2,           # simulate two orienation
 
         % some properties of simulated neurons
-        'nLayers       ': 6,  # 2 monocular + 1 binocular-summation + 2 opponency + 1 attention layer
+        'nLayers'       : 6,  # 2 monocular + 1 binocular-summation + 2 opponency + 1 attention layer
         'rectSmoothFlag': 1,  # options for half-wave rectification function. 0 is non-smoothed version; 1 is the smoothed version; both work here.
                               # if you want to implement the model using ODE solver in MATLAB (or in Mathematica), or to run the model in AUTOp07, 
                               # the smoothed version is preferred.
 
         % model parameters
-        'n_m  ': 1,     # exponent for monocualr neurons
-        'n    ': 2,     # exponent for all other neurons
+        'n_m'  : 1,     # exponent for monocualr neurons
+        'n'    : 2,     # exponent for all other neurons
         'sigma': .5,    # suppression constant (for all layer, excpet attention layer, see p.sigma_a below)
-        'm    ': 2,     # gain (scaling factor) of monocualr neurons
-        'wh   ': 2,     # weights of self-adaptation
-        'wo   ': .65,   # weights of mutual inhibition
-        'wa   ': .6,    # weights of attentional modulation
+        'm'    : 2,     # gain (scaling factor) of monocualr neurons
+        'wh'   : 2,     # weights of self-adaptation
+        'wo'   : .65,   # weights of mutual inhibition
+        'wa'   : .6,    # weights of attentional modulation
         'tau_s': 5,     # time constant for monocular and binocualr-summation neurons
                         # The range 1-10 ms has been tested, and generated similar results
         'tau_o': 20,    # time constant for opponency neurons
