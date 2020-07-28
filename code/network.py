4cd176ccdf6e2c9d0c91ae9f35347470569a5764
Lukas Neugebauer <l.neugebauer@uke.de>
2020-07-28 18:34:51 +0200

    now ises extended snapshot

--- code/network.py
+++ code/network.py
@@ -24,7 +24,7 @@ class Network(BaseClass):
         
     @property
     def snapshot(self):
-        return Snapshot(
+        return ExtendedSnapshot(
             sensory_left_1 = self.populations['sensory'].neurons['left_1'].response,
             sensory_left_2 = self.populations['sensory'].neurons['left_2'].response,
             sensory_right_1 = self.populations['sensory'].neurons['right_1'].response,
@@ -36,7 +36,37 @@ class Network(BaseClass):
             opponency_right_1 = self.populations['opponency'].neurons['right_1'].response,
             opponency_right_2 = self.populations['opponency'].neurons['right_2'].response,
             attention_1 = self.populations['attention'].neurons['1'].response,
-            attention_2 = self.populations['attention'].neurons['2'].response
+            attention_2 = self.populations['attention'].neurons['2'].response,
+            sensory_left_1_excitation = self.populations['sensory'].neurons['left_1'].excitatory_drive,
+            sensory_left_2_excitation = self.populations['sensory'].neurons['left_2'].excitatory_drive,
+            sensory_right_1_excitation = self.populations['sensory'].neurons['right_1'].excitatory_drive,
+            sensory_right_2_excitation = self.populations['sensory'].neurons['right_2'].excitatory_drive,
+            summation_1_excitation = self.populations['summation'].neurons['1'].excitatory_drive,
+            summation_2_excitation = self.populations['summation'].neurons['2'].excitatory_drive,
+            opponency_left_1_excitation = self.populations['opponency'].neurons['left_1'].excitatory_drive,
+            opponency_left_2_excitation = self.populations['opponency'].neurons['left_2'].excitatory_drive,
+            opponency_right_1_excitation = self.populations['opponency'].neurons['right_1'].excitatory_drive,
+            opponency_right_2_excitation = self.populations['opponency'].neurons['right_2'].excitatory_drive,
+            attention_1_excitation = self.populations['attention'].neurons['1'].excitatory_drive,
+            attention_2_excitation = self.populations['attention'].neurons['2'].excitatory_drive,
+            sensory_left_1_suppression = self.populations['sensory'].suppressive_drive,
+            sensory_left_2_suppression = self.populations['sensory'].suppressive_drive,
+            sensory_right_1_suppression = self.populations['sensory'].suppressive_drive,
+            sensory_right_2_suppression = self.populations['sensory'].suppressive_drive,
+            summation_1_suppression = self.populations['summation'].neurons['1'].suppressive_drive,
+            summation_2_suppression = self.populations['summation'].neurons['2'].suppressive_drive,
+            opponency_left_1_suppression = self.populations['opponency'].suppressive_drives['left'],
+            opponency_left_2_suppression = self.populations['opponency'].suppressive_drives['left'],
+            opponency_right_1_suppression = self.populations['opponency'].suppressive_drives['right'],
+            opponency_right_2_suppression = self.populations['opponency'].suppressive_drives['right'],
+            attention_1_suppression = self.populations['attention'].suppressive_drives,
+            attention_2_suppression = self.populations['attention'].suppressive_drives,
+            sensory_left_1_habituation = self.populations['sensory'].neurons['left_1'].habituation,
+            sensory_left_2_habituation = self.populations['sensory'].neurons['left_2'].habituation,
+            sensory_right_1_habituation = self.populations['sensory'].neurons['right_1'].habituation,
+            sensory_right_2_habituation = self.populations['sensory'].neurons['right_2'].habituation,
+            summation_1_habituation = self.populations['summation'].neurons['1'].habituation,
+            summation_2_habituation = self.populations['summation'].neurons['2'].habituation
         )
         
     def init_populations(
@@ -58,7 +88,6 @@ class Network(BaseClass):
         """
         timecourse = Timecourse([self.snapshot])
         for t in range(sensory_input.shape[0]):
-            print(t, sensory_input.shape[0])
             if t == sensory_input.shape[0] - 1:
                 self.one_step(sensory_input.iloc[t, :])
             else:
